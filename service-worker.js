a2d2ebbc120c7d4344bb64647e6d3cb608d4e99f
rayannbouakkaz475 <rayannbouakkaz475@gmail.com>
2026-06-10 22:29:00 +0200

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -27,3 +27,40 @@ self.addEventListener("fetch", event => {
     fetch(event.request).catch(() => caches.match(event.request).then(r => r || caches.match("./index.html")))
   );
 });
+self.addEventListener("push", event => {
+  let data = {};
+
+  try {
+    data = event.data ? event.data.json() : {};
+  } catch (e) {
+    data = {
+      title: "Coupe du Monde 2026",
+      body: event.data ? event.data.text() : "Nouvelle notification"
+    };
+  }
+
+  const title = data.title || "Coupe du Monde 2026";
+
+  const options = {
+    body: data.body || "Nouvelle notification",
+    icon: "./icon-192.png",
+    badge: "./icon-192.png",
+    data: {
+      url: data.url || "./"
+    }
+  };
+
+  event.waitUntil(
+    self.registration.showNotification(title, options)
+  );
+});
+
+self.addEventListener("notificationclick", event => {
+  event.notification.close();
+
+  const url = event.notification.data?.url || "./";
+
+  event.waitUntil(
+    clients.openWindow(url)
+  );
+});
